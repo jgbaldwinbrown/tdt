84953f540632c0bf46e5f8492929dd24ae272938
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2023-10-10 15:21:47 -0600

    pedviz and json output working

--- pkg/tdt.go
+++ pkg/tdt.go
@@ -1,6 +1,8 @@
 package tdt
 
 import (
+	"encoding/json"
+	"regexp"
 	"io"
 	"flag"
 	"math"
@@ -25,13 +27,18 @@ type Family struct {
 	FemaleF1 float64
 }
 
-func ChiSqTrioMultiFamily(fams []Family) float64 {
-	var b, c float64
+func ChiSqTrioMultiFamily(fams ...Family) float64 {
+	sums := CondenseFamilies(fams...)
+	return ChiSqTrio(sums.MaleF1, sums.FemaleF1)
+}
+
+func CondenseFamilies(fams ...Family) Family {
+	var sums Family
 	for _, f := range fams {
-		b += f.MaleF1
-		c += f.FemaleF1
+		sums.MaleF1 += f.MaleF1
+		sums.FemaleF1 += f.FemaleF1
 	}
-	return ChiSqTrio(b, c)
+	return sums
 }
 
 type PedEntry struct {
@@ -145,6 +152,30 @@ func HasX(p PedEntry, focalID int64, tree map[int64]Node) bool {
 	return false
 }
 
+func HasXFemDescent(p PedEntry, focalID int64, tree map[int64]Node) bool {
+	if p.IndividualID == focalID {
+		return true
+	}
+
+	if p.Sex == 1 {
+		return false
+	}
+
+	if p.Sex == 2 {
+		if p.MaternalID == focalID {
+			return true
+		}
+		if mom, ok := tree[p.MaternalID]; ok {
+			if HasX(mom.PedEntry, focalID, tree) {
+				return true
+			}
+		}
+		return false
+	}
+
+	return false
+}
+
 func HasAuto(p PedEntry, focalID int64, tree map[int64]Node) bool {
 	if p.IndividualID == focalID || p.PaternalID == focalID || p.MaternalID == focalID {
 		return true
@@ -206,6 +237,19 @@ func BuildFamiliesFemaleX(focalID int64, ps ...PedEntry) []Family {
 	return fams
 }
 
+func BuildFamiliesFemDescentFemaleX(focalID int64, ps ...PedEntry) []Family {
+	tree := BuildPedTree(ps...)
+	var fams []Family
+	for _, p := range ps {
+		if HasXFemDescent(p, focalID, tree) {
+			if p.Sex == 2 {
+				fams = AddFam(fams, p, tree)
+			}
+		}
+	}
+	return fams
+}
+
 func BuildFamiliesAuto(focalID int64, ps ...PedEntry) []Family {
 	tree := BuildPedTree(ps...)
 	var fams []Family
@@ -235,7 +279,16 @@ func ParsePedEntry(s string) (PedEntry, error) {
 		return p, fmt.Errorf("len(line) %v < 6", len(line))
 	}
 	_, e := Scan(line, &p.FamilyID, &p.IndividualID, &p.PaternalID, &p.MaternalID, &p.Sex, &p.Phenotype)
-	return p, e
+	if e != nil {
+		return p, fmt.Errorf("ParsePedEntry: %w; line: %v", e, line)
+	}
+	return p, nil
+}
+
+var skipRe = regexp.MustCompile(`^#|^$`)
+
+func ShouldSkipPedLine(line string) bool {
+	return skipRe.MatchString(line)
 }
 
 func ParsePed(r io.Reader) ([]PedEntry, error) {
@@ -245,6 +298,10 @@ func ParsePed(r io.Reader) ([]PedEntry, error) {
 		if s.Err() != nil {
 			return nil, s.Err()
 		}
+		if ShouldSkipPedLine(s.Text()) {
+			continue
+		}
+
 		p, e := ParsePedEntry(s.Text())
 		if e != nil {
 			return nil, e
@@ -260,7 +317,75 @@ func Must(e error) {
 	}
 }
 
-func FullTDTTest() {
+type TDTResult struct {
+	Name string
+	Totals Family
+	Nfamilies float64
+	MaleProportion float64
+	MeanMalesPerFam float64
+	MeanFemalesPerFam float64
+	MeanChildrenPerFam float64
+	Chisq float64
+	P float64
+}
+
+func TDTTest(fams ...Family) TDTResult {
+	var r TDTResult
+
+	r.Totals = CondenseFamilies(fams...)
+	r.Chisq = ChiSqTrio(r.Totals.MaleF1, r.Totals.FemaleF1)
+	r.MaleProportion = r.Totals.MaleF1 / (r.Totals.MaleF1 + r.Totals.FemaleF1)
+
+	dist := distuv.ChiSquared{K: 1}
+	r.P = 1 - dist.CDF(math.Abs(r.Chisq))
+
+	r.Nfamilies = float64(len(fams))
+	r.MeanMalesPerFam = r.Totals.MaleF1 / r.Nfamilies
+	r.MeanFemalesPerFam = r.Totals.FemaleF1 / r.Nfamilies
+	r.MeanChildrenPerFam = (r.Totals.FemaleF1 + r.Totals.MaleF1) / r.Nfamilies
+
+	return r
+}
+
+type TDTResultJson struct {
+	Name string
+	TotalMales any
+	TotalFemales any
+	Nfamilies any
+	MaleProportion any
+	MeanMalesPerFam any
+	MeanFemalesPerFam any
+	MeanChildrenPerFam any
+	Chisq any
+	P any
+}
+
+func FloatToJson(f float64) any {
+	if math.IsNaN(f) {
+		return "NaN"
+	}
+	if math.IsInf(f, 0) {
+		return "Infinity"
+	}
+	return f
+}
+
+func ToJson(r TDTResult) TDTResultJson {
+	var j TDTResultJson
+	j.Name = r.Name
+	j.TotalMales = FloatToJson(r.Totals.MaleF1)
+	j.TotalFemales = FloatToJson(r.Totals.FemaleF1)
+	j.Nfamilies = FloatToJson(r.Nfamilies)
+	j.MaleProportion = FloatToJson(r.MaleProportion)
+	j.MeanMalesPerFam = FloatToJson(r.MeanMalesPerFam)
+	j.MeanFemalesPerFam = FloatToJson(r.MeanFemalesPerFam)
+	j.MeanChildrenPerFam = FloatToJson(r.MeanChildrenPerFam)
+	j.Chisq = FloatToJson(r.Chisq)
+	j.P = FloatToJson(r.P)
+	return j
+}
+
+func FullTDTTestOld() {
 	focal := flag.Int("f", -1, "focal ID (required)")
 	flag.Parse()
 	if *focal == -1 {
@@ -272,15 +397,54 @@ func FullTDTTest() {
 
 	dist := distuv.ChiSquared{K: 1}
 
-	xchi := ChiSqTrioMultiFamily(BuildFamiliesFemaleX(int64(*focal), peds...))
+	xchi := ChiSqTrioMultiFamily(BuildFamiliesFemaleX(int64(*focal), peds...)...)
 	xp := 1 - dist.CDF(math.Abs(xchi))
 	fmt.Printf("xchi: %v; xp: %v\n", xchi, xp)
 
-	ychi := ChiSqTrioMultiFamily(BuildFamiliesY(int64(*focal), peds...))
+	xchifemdescent := ChiSqTrioMultiFamily(BuildFamiliesFemDescentFemaleX(int64(*focal), peds...)...)
+	xpfemdescent := 1 - dist.CDF(math.Abs(xchi))
+	fmt.Printf("fem descent xchi: %v; xp: %v\n", xchifemdescent, xpfemdescent)
+
+	ychi := ChiSqTrioMultiFamily(BuildFamiliesY(int64(*focal), peds...)...)
 	yp := 1 - dist.CDF(math.Abs(ychi))
 	fmt.Printf("ychi: %v; yp: %v\n", ychi, yp)
 
-	achi := ChiSqTrioMultiFamily(BuildFamiliesAuto(int64(*focal), peds...))
+	achi := ChiSqTrioMultiFamily(BuildFamiliesAuto(int64(*focal), peds...)...)
 	ap := 1 - dist.CDF(math.Abs(achi))
 	fmt.Printf("achi: %v; ap: %v\n", achi, ap)
+}
+
+
+func FullTDTTest() {
+	focal := flag.Int("f", -1, "focal ID (required)")
+	flag.Parse()
+	if *focal == -1 {
+		panic(fmt.Errorf("missing -f"))
+	}
+
+	peds, e := ParsePed(os.Stdin)
+	Must(e)
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "\t")
+
+	res := TDTTest(BuildFamiliesFemaleX(int64(*focal), peds...)...)
+	res.Name = "FemaleX"
+	err := enc.Encode(ToJson(res))
+	Must(err)
+
+	res = TDTTest(BuildFamiliesFemDescentFemaleX(int64(*focal), peds...)...)
+	res.Name = "FemDescentFemaleX"
+	err = enc.Encode(ToJson(res))
+	Must(err)
+
+	res = TDTTest(BuildFamiliesY(int64(*focal), peds...)...)
+	res.Name = "Y"
+	err = enc.Encode(ToJson(res))
+	Must(err)
+
+	res = TDTTest(BuildFamiliesAuto(int64(*focal), peds...)...)
+	res.Name = "Auto"
+	err = enc.Encode(ToJson(res))
+	Must(err)
 }
